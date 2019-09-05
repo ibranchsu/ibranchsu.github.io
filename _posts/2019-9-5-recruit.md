c54fda777cdb3946779b371038b18b1a7b8cf32b
Lizhen Liang <liamliang011235@gmail.com>
2019-09-05 11:39:13 -0400

    Recruit

---
+++ _posts/2019-9-5-recruit.md
@@ -0,0 +1,11 @@
+---
+title: "We are Looking for New Members"
+permalink: /recruit/
+---
+
+![Recruit](/images/recruit.png)   
+
+iBranch 大家庭开始招新啦!  
+我们致力于帮助大家更好的适应美国的学习生活，搭建信息交流平台。  
+如果你也有一样的想法，请加入我们！  
+点击[链接](https://docs.google.com/forms/d/e/1FAIpQLSdUtoeA4T6VMN1epciV3ntv-NUYQtMI3FYUyb0gJjvvhv2Zaw/viewform?usp=sf_link)报名并发送简历至邮箱ibranchsyracuse@gmail.com
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,46 @@
+var store = [{
+        "title": "Luggage Guide",
+        "excerpt":"Important list: Passport Visa card Driving license I20 Toefl，GRE，GMAT Transcripts Certificate of Health Examination International Certificate of Vaccination School offer Undergraduate Transcript All the materials you prepared for the visa Crash Spare glasses Tissue Neck pillow What you may need in your checked luggage Syracuse is close to the Great...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/luggageguide/",
+        "teaser":null},{
+        "title": "Mobile Plan Guide",
+        "excerpt":"There are three mainstream communication operators in the US: Verizon、AT&amp;T、T-Mobile, Sprint. Verizon Verizon has the strongest signal, but it is also expensive. Data traffic packages are divided into S, M, L, XL, XXL packages, L-level packages and above can have a Bonus per Line. Compared with AT&amp;T, higher price and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/mobileguide/",
+        "teaser":null},{
+        "title": "Flight Guide",
+        "excerpt":"Booking guide Freshmen can check the ticket on the relevant website in advance when purchasing tickets. Generally speaking, tickets should be booked 3-6 weeks in advance. Starting dates for spring and fall semesters are different. The recommended time is to arrive in Syracuse should be one week before class. Most...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/flightguide/",
+        "teaser":null},{
+        "title": "Housing Guide: Within Walking Distance to Campus",
+        "excerpt":"The content was originally created by iGrad and now edited by iBranch. Follwing is a list of housing options within walking distance to campus. Information was gathered from apartment website and current students’ feedback. Special thanks to all current students who have kindly provided valuable information. 502 University Apartments Address:...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/housingguide2/",
+        "teaser":null},{
+        "title": "Housing Guide: Longer Distance to Campus",
+        "excerpt":"The content was originally created by iGrad and now edited by iBranch. Following is a list of housing options that have longer distance to campus (not within walking distance). Information was gathered from apartment website and current students’ feedback. Special thanks to all current students who have kindly provided valuable...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/housingguide/",
+        "teaser":null},{
+        "title": "Dental and Vision Plans",
+        "excerpt":"Full-time, matriculated graduate and law students are eligible for the graduate student dental and vision insurance plans. The 2019-20 plans’ features will be available soon and posted to this page. Pricing information is as follows: 2019-20 Dental Preventative Low Plan (Monthly Rates) Grad/law student only – $17.35 Grad/law student and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/dentalvision/",
+        "teaser":null},{
+        "title": "Student Health Plan",
+        "excerpt":"Student Health Insurance Plan Eligibility and Coverage All full-time, matriculated undergraduate, graduate and law students are eligible for the Student Health Insurance Plan. Once per academic year, eligible students must enroll in or waive out of the Student Health Insurance Plan with alternate qualifying coverage through their MySlice account. The...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/healthinsurance/",
+        "teaser":null},{
+        "title": "Restaurants You Should Definately Try Out in Syracuse",
+        "excerpt":"Yang Di Chun BBQ Address: 2042 Erie Blvd E, Syracuse, NY 13224 Phone Number : (315) 991-4619 Overall Rate: ★★★★☆ Food Rate: ★★★★☆ Environment Rate: ★★★★☆ Service Rate: ★★★☆☆ Old Chengdu Café Address: 1113 E Fayette St, Syracuse, NY 13210 Phone Number : (315) 428-8899 Overall Rate: ★★★★☆ Food Rate:...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/foodguide/",
+        "teaser":null},{
+        "title": "We are Looking for New Members",
+        "excerpt":"   iBranch 大家庭开始招新啦!  我们致力于帮助大家更好的适应美国的学习生活，搭建信息交流平台。  如果你也有一样的想法，请加入我们！  点击链接报名并发送简历至邮箱ibranchsyracuse@gmail.com  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/recruit/",
+        "teaser":null}]
